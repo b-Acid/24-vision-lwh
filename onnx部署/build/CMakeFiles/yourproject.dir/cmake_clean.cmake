b5faf0c73fac4a8c6a9b6dce1db9ab5d97c8f091
b-Acid <145821466+b-Acid@users.noreply.github.com>
2023-10-06 23:07:15 +0800

    update

---
+++ onnx部署/build/CMakeFiles/yourproject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yourproject.dir/test.cpp.o"
+  "CMakeFiles/yourproject.dir/test.cpp.o.d"
+  "yourproject"
+  "yourproject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yourproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
